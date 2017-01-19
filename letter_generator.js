373c4e8dff3dd4989699b75143415b41855ebeb9
Rob Seward <robseward@gmail.com>
2017-01-18 23:25:39 -0500

    Add letter_generator module

---
+++ letter_generator.js
@@ -0,0 +1,64 @@
+var letterGenerator = (function() {
+  var exports = {}
+  exports.randomLetters = function(length)
+  {
+      var letterArray = []
+      for( var i=0; i < length; i++ )
+          letterArray.push( exports.randomLetterWeighted())
+      return letterArray;
+  }
+
+  // letter frequencies from https://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+  var letterFrequencies = {
+  "E" :	0.1202,
+  "T" :	0.0910,
+  "A" :	0.0812,
+  "O" :	0.0768,
+  "I" :	0.0731,
+  "N" :	0.0695,
+  "S" :	0.0628,
+  "R" :	0.0602,
+  "H" :	0.0592,
+  "D" :	0.0432,
+  "L" :	0.0398,
+  "U" :	0.0288,
+  "C" :	0.0271,
+  "M" :	0.0261,
+  "F" :	0.0230,
+  "Y" :	0.0211,
+  "W" :	0.0209,
+  "G" :	0.0203,
+  "P" :	0.0182,
+  "B" :	0.0149,
+  "V" :	0.0111,
+  "K" :	0.0069,
+  "X" :	0.0017,
+  "Q" :	0.0011,
+  "J" :	0.0010,
+  "Z" :	0.0007,
+  }
+
+  function createCumlativeFrequency(letterFrequencies) {
+    var cumlativeFrequencies = {}
+    var cumulation = 0.0
+    for( var letter in letterFrequencies) {
+      let prob = letterFrequencies[letter]
+      cumulation += prob
+      cumlativeFrequencies[letter] = cumulation
+    }
+    return cumlativeFrequencies
+  }
+
+  cumulativeFrequencies = createCumlativeFrequency(letterFrequencies)
+
+  exports.randomLetterWeighted = function() {
+    var n = Math.random()
+    for(var letter in cumulativeFrequencies) {
+      if (n < cumulativeFrequencies[letter]) {
+        return letter
+      }
+    }
+  }
+
+  return exports
+}());
