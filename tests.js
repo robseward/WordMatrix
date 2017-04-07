5227f0af6ee31c10ad6af15636fd16ca97b514bc
Rob Seward <rob@electricobjects.com>
2017-04-07 18:48:16 -0400

    Sketching out word finding tests

--- tests.js
+++ tests.js
@@ -1,9 +1,36 @@
 var elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+function elementStubFactory(letterList) {
+  var elements = []
+  for(var i=0; i < letterList.length; i++){
+    var e = new Element(letterList[i], 0)
+    elements.push(e)
+  }
+  return elements
+}
 
-function testMatrixRows() {
-  var expectedRows = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
-  let matrix = new Matrix().setMatrix(5, 2, elements)
-  console.log(matrix.matrix)
-  return (matrix.matrix.equals(expectedRows))
+function testWordFinding(){
+
+
+  function testCompoundWords() {
+    var baseball = elementStubFactory("BASEBALL")
+    var crosswalk = elementStubFactory("CROSSWALK")
+    var anybody = elementStubFactory("ANYBODY")
+
+    var results = findWords(baseball)
+    if (results != ["base"]) {
+      throw "failed test: " + getWords(results)
+    }
+  }
+
+  function getWords(results) {
+    output = []
+    for (var i=0; i < results.length; i++) {
+      output.push(results[i].word)
+    }
+    return output
+  }
+
+
+  testCompoundWords()
 }
